1159980a8aaec72510206e30aa9ffbd5cb43e2c4
Péter Attila Rigó <61515182+rigopeti96@users.noreply.github.com>
2022-11-19 15:11:54 +0100

    Redundant codes remove

---
+++ src/test/kotlin/hu/aut/bme/report_rest_springdata/ReportRestSpringDataApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.aut.bme.report_rest_springdata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReportRestSpringDataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
