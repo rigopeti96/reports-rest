bea2a2ee48f82c8477bc1683becac238898b0a92
Péter Attila Rigó <61515182+rigopeti96@users.noreply.github.com>
2023-12-12 19:28:19 +0100

    Web page done

--- src/main/kotlin/hu/aut/bme/report_rest_springdata/controller/FileController.kt
+++ src/main/kotlin/hu/aut/bme/report_rest_springdata/controller/FileController.kt
@@ -2,21 +2,21 @@ package hu.aut.bme.report_rest_springdata.controller
 
 import hu.aut.bme.report_rest_springdata.collections.Station
 import hu.aut.bme.report_rest_springdata.collections.Stops
+import hu.aut.bme.report_rest_springdata.controller.FileController.Companion.logger
 import hu.aut.bme.report_rest_springdata.exception.InsertZipIntoDestinationFolderException
 import hu.aut.bme.report_rest_springdata.gtfshandler.CSVParser
 import hu.aut.bme.report_rest_springdata.gtfshandler.Unzipper
 import hu.aut.bme.report_rest_springdata.repository.StationRepository
 import hu.aut.bme.report_rest_springdata.repository.StopRepository
 import hu.aut.bme.report_rest_springdata.repository.UserRepository
+import org.apache.commons.collections.map.MultiValueMap
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.http.*
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.IOException
@@ -33,33 +33,40 @@ class FileController {
 
     @Autowired
     private lateinit var stationRepository: StationRepository
+
     /**
      * Get the new BKK GTFS file and undzip it
      * @param uploadedZip: the new ZIP archive uploaded in webapp
      * @return HTTPStatus.OK if upload was successful
      */
+    @CrossOrigin
     @PostMapping("/uploadArchive")
     @PreAuthorize("hasRole('WEBUSER')")
     fun uploadArchive(
-        @RequestParam uploadedZip: MultipartFile
+        @RequestHeader file: MultipartFile
     ): ResponseEntity<String>{
-        println("zip upload called")
+
+        //val encoded = file.bytes
+
+        logger.info("zip upload called")
         try {
-            insertZipIntoTargetFolder(uploadedZip)
+            logger.info("Is uploadedZip empty: ${file.isEmpty}")
+            insertZipIntoTargetFolder(file)
         } catch (e: InsertZipIntoDestinationFolderException){
-            println(e.stackTrace)
+            e.printStackTrace()
             return ResponseEntity(e.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
 
         return try {
             Unzipper.unzip("actualGtfsData.zip")
             stopRepository.deleteAll()
-            val stationList = stationRepository.findAll()
+            logger.info("GTFS endpoint called")
+//            val stationList = stationRepository.findAll()
             val stops: MutableList<Stops> = CSVParser.readLineByLineExample("stops.txt")
-            for(i in 0 until stops.size){
+            /*for(i in 0 until stops.size){
                 val actStop = stops[i]
                 actStop.type = findBaseStationType(stationList, actStop.stop_lat, actStop.stop_lon)
-            }
+            }*/
             saveStops(stops)
             ResponseEntity("Upload was successful", HttpStatus.OK)
         } catch (e: IOException){
@@ -91,6 +98,17 @@ class FileController {
 
         return ""
     }
+
+    private fun getByteArrayEntity(fName: String, fData: ByteArray) =
+        HttpEntity(
+            object: ByteArrayResource(fData){
+                override fun getFilename() =
+                    "$fName.zip"
+            },
+            HttpHeaders().also {
+                it.contentType= MediaType.APPLICATION_PDF
+            }
+        )
 
     /**
      * Try to insert zip file into the destination folder
